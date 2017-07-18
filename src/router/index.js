d95e9167494abd702feab2eea40097549c2bca0f
ly2011 <1518550424@qq.com>
2017-07-18 23:51:17 +0800

    修改routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,81 +1,84 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue'
+import Router from 'vue-router'
 
-import Index from "../views/index";
-import Login from "../views/login";
-import My from "../views/index/my";
-import songList from "../views/index/discover/songList";
-import Recommend from "../views/index/discover/recommend";
-import Discover from "../views/index/discover";
+import Index from '../views/index'
+import Login from '../views/login'
+import My from '../views/index/my'
+import songList from '../views/index/discover/songList'
+import Recommend from '../views/index/discover/recommend'
+import Discover from '../views/index/discover'
 
-Vue.use(Router);
+Vue.use(Router)
 
 const router = new Router({
-  mode: "hash",
+  mode: 'history',
   base: __dirname,
-  scrollBehavior(to, from, savedPosition) {
-    console.log('from: ', from);
+  scrollBehavior (to, from, savedPosition) {
+    // 只在history模式下生效
+    console.log('from: ', from)
     if (savedPosition) {
-      return savedPosition;
+      return savedPosition
     } else {
       if (from.meta.keepAlive) {
-        from.meta.savedPosition = document.body.scrollTop;
+        from.meta.savedPosition = document.body.scrollTop
       }
-      return { x: 0, y: to.meta.savedPosition || 0 };
+      return { x: 0, y: to.meta.savedPosition || 0 }
     }
   },
   routes: [
     {
-      path: "",
+      path: '',
       // name: 'index',
       redirect: '/index'
     },
     {
-      path: "/index",
-      name: "index",
+      path: '/index',
+      name: 'index',
       component: Index,
       children: [
         {
-          path: "my",
-          name: "my",
+          path: 'my',
+          name: 'my',
           component: My,
           meta: {
             keepAlive: true
           }
         },
         {
-          path: "discover",
-          redirect: "/index/discover/songList"
-        },
-        {
-          path: "discover",
-          name: "discover",
+          path: 'discover',
+          name: 'discover',
           component: Discover,
           children: [
             {
-              path: "songList",
-              name: "songList",
-              component: songList
+              path: 'songList',
+              name: 'songList',
+              component: songList,
+              meta: {
+                keepAlive: true
+              }
             },
             {
-              path: "recommend",
-              name: "recommend",
-              component: Recommend
+              path: 'recommend',
+              name: 'recommend',
+              component: Recommend,
+              meta: {
+                keepAlive: true
+              }
             }
           ]
         }
       ]
     },
     {
-      path: "/login",
-      name: "login",
+      path: '/login',
+      name: 'login',
       component: Login
     }
   ]
-});
+})
 
 router.afterEach((from, to) => {
-  console.log(from.name);
-});
+  console.log(from.name)
+})
 
-export default router;
+export default router
