c35c3ade366aa4892fd1bf9688318dcad461b8e5
ly2011 <1518550424@qq.com>
2017-07-25 11:46:45 +0800

    添加对 .vue 文件的验证

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,14 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 7,
     sourceType: 'module', //类型为module，因为代码使用了使用了ECMAScript模块,
-    allowImportExportEverywhere: false
+    allowImportExportEverywhere: false,
+    ecmaFeatures: {
+      "experimentalObjectRestSpread": true
+    }
   },
   env: {
     browser: true //预定义的全局变量，这里是浏览器环境
   },
-  extends: ['plugin:vue-libs/recommended'],
-  plugins: ['react', 'jsx-a11y', 'import']
+  extends: [''],
+  plugins: ['react', 'jsx-a11y', 'import', 'html']
 };
