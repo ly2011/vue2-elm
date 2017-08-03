b219c6ebb59bba5ac94469c92400836e09a0079c
ly2011 <1518550424@qq.com>
2017-08-03 20:17:37 +0800

    删除多余的文件

--- src/views/index.vue
+++ src/views/index.vue
@@ -237,6 +237,61 @@
         </el-option-group>
       </el-select>
     </div>
+
+    <div class="block">
+      <span class="demonstration">默认click触发子菜单</span>
+      <el-cascader
+      :options="options4"
+      v-model="selectedOptions"
+      @change="handleChange"
+      >
+
+      </el-cascader>
+    </div>
+
+    <div class="block">
+      <el-switch
+        v-model="switch1"
+        on-text=""
+        off-text=""
+      >
+      </el-switch>
+      <el-switch
+        v-model="switch2"
+        on-color="#13ce66"
+        off-color="#ff4949"
+        disabled
+      >
+      </el-switch>
+
+    </div>
+
+    <div class="block">
+      <el-tooltip
+      :content="'Switch value: ' + switch3"
+      placement="top"
+      >
+        <el-switch
+          v-model="switch3"
+          on-color="#13ce66"
+          off-color="#ff4949"
+          on-value="100"
+          off-value="0"
+        >
+
+        </el-switch>
+      </el-tooltip>
+    </div>
+    <div class="block">
+      <el-slider
+        v-model="slider1"
+        :step="10"
+        vertical
+        show-stops
+        height="200px"
+      >
+      </el-slider>
+    </div>
   </div>
 </template>
 <script>
@@ -258,7 +313,11 @@ import {
   Select,
   Option,
   OptionGroup,
-  Autocomplete
+  Autocomplete,
+  Cascader,
+  Switch,
+  Tooltip,
+  Slider
 } from 'element-ui'
 Vue.use(Button)
 Vue.use(Dialog)
@@ -276,6 +335,10 @@ Vue.use(Select)
 Vue.use(Option)
 Vue.use(OptionGroup)
 Vue.use(Autocomplete)
+Vue.use(Cascader)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Slider)
 
 Vue.component('my-item-zh', {
   functional: true,
@@ -395,7 +458,207 @@ export default {
             label: '大连'
           }]
         }
-      ]
+      ],
+      options4: [{
+          value: 'zhinan',
+          label: '指南',
+          disabled: true,
+          children: [{
+            value: 'shejiyuanze',
+            label: '设计原则',
+            children: [{
+              value: 'yizhi',
+              label: '一致'
+            }, {
+              value: 'fankui',
+              label: '反馈'
+            }, {
+              value: 'xiaolv',
+              label: '效率'
+            }, {
+              value: 'kekong',
+              label: '可控'
+            }]
+          }, {
+            value: 'daohang',
+            label: '导航',
+            children: [{
+              value: 'cexiangdaohang',
+              label: '侧向导航'
+            }, {
+              value: 'dingbudaohang',
+              label: '顶部导航'
+            }]
+          }]
+        }, {
+          value: 'zujian',
+          label: '组件',
+          children: [{
+            value: 'basic',
+            label: 'Basic',
+            children: [{
+              value: 'layout',
+              label: 'Layout 布局'
+            }, {
+              value: 'color',
+              label: 'Color 色彩'
+            }, {
+              value: 'typography',
+              label: 'Typography 字体'
+            }, {
+              value: 'icon',
+              label: 'Icon 图标'
+            }, {
+              value: 'button',
+              label: 'Button 按钮'
+            }]
+          }, {
+            value: 'form',
+            label: 'Form',
+            children: [{
+              value: 'radio',
+              label: 'Radio 单选框'
+            }, {
+              value: 'checkbox',
+              label: 'Checkbox 多选框'
+            }, {
+              value: 'input',
+              label: 'Input 输入框'
+            }, {
+              value: 'input-number',
+              label: 'InputNumber 计数器'
+            }, {
+              value: 'select',
+              label: 'Select 选择器'
+            }, {
+              value: 'cascader',
+              label: 'Cascader 级联选择器'
+            }, {
+              value: 'switch',
+              label: 'Switch 开关'
+            }, {
+              value: 'slider',
+              label: 'Slider 滑块'
+            }, {
+              value: 'time-picker',
+              label: 'TimePicker 时间选择器'
+            }, {
+              value: 'date-picker',
+              label: 'DatePicker 日期选择器'
+            }, {
+              value: 'datetime-picker',
+              label: 'DateTimePicker 日期时间选择器'
+            }, {
+              value: 'upload',
+              label: 'Upload 上传'
+            }, {
+              value: 'rate',
+              label: 'Rate 评分'
+            }, {
+              value: 'form',
+              label: 'Form 表单'
+            }]
+          }, {
+            value: 'data',
+            label: 'Data',
+            children: [{
+              value: 'table',
+              label: 'Table 表格'
+            }, {
+              value: 'tag',
+              label: 'Tag 标签'
+            }, {
+              value: 'progress',
+              label: 'Progress 进度条'
+            }, {
+              value: 'tree',
+              label: 'Tree 树形控件'
+            }, {
+              value: 'pagination',
+              label: 'Pagination 分页'
+            }, {
+              value: 'badge',
+              label: 'Badge 标记'
+            }]
+          }, {
+            value: 'notice',
+            label: 'Notice',
+            children: [{
+              value: 'alert',
+              label: 'Alert 警告'
+            }, {
+              value: 'loading',
+              label: 'Loading 加载'
+            }, {
+              value: 'message',
+              label: 'Message 消息提示'
+            }, {
+              value: 'message-box',
+              label: 'MessageBox 弹框'
+            }, {
+              value: 'notification',
+              label: 'Notification 通知'
+            }]
+          }, {
+            value: 'navigation',
+            label: 'Navigation',
+            children: [{
+              value: 'menu',
+              label: 'NavMenu 导航菜单'
+            }, {
+              value: 'tabs',
+              label: 'Tabs 标签页'
+            }, {
+              value: 'breadcrumb',
+              label: 'Breadcrumb 面包屑'
+            }, {
+              value: 'dropdown',
+              label: 'Dropdown 下拉菜单'
+            }, {
+              value: 'steps',
+              label: 'Steps 步骤条'
+            }]
+          }, {
+            value: 'others',
+            label: 'Others',
+            children: [{
+              value: 'dialog',
+              label: 'Dialog 对话框'
+            }, {
+              value: 'tooltip',
+              label: 'Tooltip 文字提示'
+            }, {
+              value: 'popover',
+              label: 'Popover 弹出框'
+            }, {
+              value: 'card',
+              label: 'Card 卡片'
+            }, {
+              value: 'carousel',
+              label: 'Carousel 走马灯'
+            }, {
+              value: 'collapse',
+              label: 'Collapse 折叠面板'
+            }]
+          }]
+        }, {
+          value: 'ziyuan',
+          label: '资源',
+          children: [{
+            value: 'axure',
+            label: 'Axure Components'
+          }, {
+            value: 'sketch',
+            label: 'Sketch Templates'
+          }, {
+            value: 'jiaohu',
+            label: '组件交互文档'
+          }]
+        }],
+        switch1: true,
+        switch2: true,
+        switch3: '100',
+        slider1: 0
     }
   },
   mounted() {
