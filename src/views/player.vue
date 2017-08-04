948b7a0295b9c0bda8a159a78e4da59b9e597e5b
ly2011 <1518550424@qq.com>
2017-08-04 17:49:20 +0800

    添加form校验

--- src/views/player.vue
+++ src/views/player.vue
@@ -1,34 +1,99 @@
 <template>
   <div>
-    <el-form ref="form" label-width="80px">
-      <el-form-item label="活动名称">
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+      <el-form-item label="活动名称" prop="name">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
-      <el-form-item label="活动区域">
+      <el-form-item label="活动区域" prop="region">
         <el-select v-model="form.region" placeholder="请选择活动区域">
           <el-option label="区域一" value="shanghai"></el-option>
           <el-option label="区域二" value="guangzhou"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="活动时间">
+      <el-form-item label="活动时间" required>
         <el-col :span="11">
-          <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+          <el-form-item prop="date1">
+            <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+          </el-form-item>
+
         </el-col>
         <el-col class="line" :span="2">-</el-col>
         <el-col :span="11">
-          <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+          <el-form-item prop="date2">
+            <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+          </el-form-item>
+
         </el-col>
       </el-form-item>
 
-      <el-form-item label="即时配送">
+      <el-form-item label="即时配送" prop="delivery">
         <el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
+      </el-form-item>
+
+      <el-form-item label="活动性质" prop="type">
+        <el-checkbox-group v-model="form.type">
+          <el-checkbox label="美食/餐厅线上活动"></el-checkbox>
+          <el-checkbox label="地推活动"></el-checkbox>
+          <el-checkbox label="线下主题活动"></el-checkbox>
+          <el-checkbox label="单纯品牌曝光"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+
+      <el-form-item label="特殊资源" prop="resource">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="线上品牌赞助商"></el-radio>
+          <el-radio label="线下场地免费"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <el-form-item label="活动形式" prop="desc">
+        <el-input type="textarea" v-model="form.desc"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('form')">立即创建</el-button>
+        <el-button @click="resetForm('form')">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-radio-group v-model="labelPosition" size="small">
+      <el-radio-button label="left">左对齐</el-radio-button>
+      <el-radio-button label="right">右对齐</el-radio-button>
+      <el-radio-button label="top">顶部对齐</el-radio-button>
+    </el-radio-group>
+    <div style="margin: 20px"></div>
+
+    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="名称">
+        <el-input v-model="formLabelAlign.name"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-input v-model="formLabelAlign.region"></el-input>
+      </el-form-item>
+      <el-form-item label="活动形式">
+        <el-input v-model="formLabelAlign.type"></el-input>
       </el-form-item>
     </el-form>
   </div>
 </template>
 <script>
 import Vue from 'vue'
-import {Form, FormItem,Input, Select,Option,Col,Row,DatePicker,TimePicker,Switch } from 'element-ui'
+import {Form, FormItem,Input, Select,Option,Col,Row,DatePicker,TimePicker,Switch, Checkbox, CheckboxGroup,Radio,RadioGroup, Button, RadioButton } from 'element-ui'
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
@@ -39,6 +104,12 @@ Vue.use(Row)
 Vue.use(DatePicker)
 Vue.use(TimePicker)
 Vue.use(Switch)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(Button)
+Vue.use(RadioButton)
 export default {
   data() {
     return {
@@ -51,7 +122,59 @@ export default {
         type: [],
         resource: '',
         desc: ''
+      },
+      formInline: {
+        user: '',
+        region: ''
+      },
+      labelPosition: 'right',
+      formLabelAlign: {
+        name: '',
+        region: '',
+        type: ''
+      },
+      rules: {
+        name: [
+          {required: true,message: '请输入活动名称', trigger: 'blur'},
+          {min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur'}
+        ],
+        region: [
+          {required: true,message: '请选择活动区域', trigger: 'change'}
+        ],
+        date1: [
+          {type: 'date', required: true, message: '请选择日期', trigger: 'change'}
+        ],
+        date2: [
+          {type: 'date', required: true, message: '请选择时间', trigger: 'change'}
+        ],
+        type: [
+          {type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change'}
+        ],
+        resource: [
+          {required: true, message: '请选择活动资源', trigger: 'change'}
+        ],
+        desc: [
+          {required: true, message: '请填写活动形式', trigger: 'blur'}
+        ]
       }
+    }
+  },
+  methods: {
+    onSubmit() {
+
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
     }
   }
 }
